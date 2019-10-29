3e34b725d83e893f9c9448ab5f43062c8f75908e
RadipalyCatherine <54911943+RadipalyCatherine@users.noreply.github.com>
2019-10-29 14:10:55 -0400

    Travail du 29/10/2019
    -Création du chapitre 07 ACTUNEWS
    -Création du sous dossier "includes"
    -Création du fichier PHP "header.php"
    -Réalisation d'un Exercice pratique à l'aide d'une vidéo Youtube "Trappes-PHP Jour 8"

---
+++ 07-ACTUNEWS/includes/header.php
@@ -0,0 +1,37 @@
+<?php
+    $categories = ['Politique', 'Economie', 'Culture', 'Sport'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Actunews | Premier site d'actualité en France</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+
+<!-- Menu du site -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Actunews | Premier site d'actualité en France</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">
+        Accueil <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Fin du Menu du site -->
+</body>
+</html>
---
+++ 07-ACTUNEWS/index.php
@@ -0,0 +1,20 @@
+<?php
+// Inclusion de header.php sur la page.
+require_once(__DIR__.'/includes/header.php');
+?>
+
+<!-- Ici, viendra le contenu de ma page -->
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <h1>Page d'accueil</h1>
+        </div>
+        </div>
+</div>
+
+<?php
+// Inclusion de footer.php sur la page.
+require_once(__DIR__.'/includes/footer.php');
+?>
+
+
