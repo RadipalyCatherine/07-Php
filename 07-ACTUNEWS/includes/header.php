38a7ee169d10fadc5eef98ac431466b8d3b5b328
RadipalyCatherine <54911943+RadipalyCatherine@users.noreply.github.com>
2019-10-30 15:27:51 -0400

    Travail du 30/10/2019 2e partie

--- 07-ACTUNEWS/includes/header.php
+++ 07-ACTUNEWS/includes/header.php
@@ -19,7 +19,7 @@
 
 <!-- Menu du site -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Actunews | Premier site d'actualité en France</a>
+  <a class="navbar-brand" href="#">ActuNews | Premier site d'actualité en France</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
